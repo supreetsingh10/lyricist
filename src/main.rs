aa66b5922f63e76996f424af4a91df630c7bd63e
Supreet <singh.supreet14@gmail.com>
2024-09-23 21:12:14 +0530

    Major refactor fixes related to layout, rendering

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 mod keyboard_event;
 use core::panic;
-use std::io::{stdout, Result};
-use std::rc::Rc;
-
 use crossterm::event::{KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use keyboard_event::{handle_keyboard_events, KeyPressEvent};
 use layout::Layout;
@@ -14,79 +11,134 @@ use ratatui::{
     prelude::*,
     widgets::*,
 };
+use std::rc::Rc;
+use std::{
+    collections::HashMap,
+    io::{stdout, Result},
+};
 
 #[allow(dead_code)]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 enum KeyLength {
     SHORT,
     MEDIUM,
     LONG,
 }
 
+type Coord = (u32, u32);
 #[allow(dead_code)]
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 struct Key {
-    keychar: char,
-    keylength: KeyLength,
+    key_code: KeyCode,
+    key_length: KeyLength,
 }
 
 impl Key {
-    fn from_values(l_keychar: char, l_keylength: KeyLength) -> Self {
+    fn from_values(keycode: KeyCode, keylength: KeyLength) -> Self {
         Key {
-            keychar: l_keychar,
-            keylength: l_keylength,
+            key_code: keycode,
+            key_length: keylength,
         }
     }
 }
 
-fn initialize_keys() -> Vec<Vec<Key>> {
+fn initialize_key_vec() -> Vec<Vec<Key>> {
     vec![
         vec![
-            Key::from_values('0', KeyLength::SHORT),
-            Key::from_values('1', KeyLength::SHORT),
-            Key::from_values('2', KeyLength::SHORT),
-            Key::from_values('3', KeyLength::SHORT),
-            Key::from_values('4', KeyLength::SHORT),
-            Key::from_values('5', KeyLength::SHORT),
-            Key::from_values('6', KeyLength::SHORT),
-            Key::from_values('7', KeyLength::SHORT),
-            Key::from_values('8', KeyLength::SHORT),
-            Key::from_values('9', KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('0'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('1'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('2'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('3'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('4'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('5'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('6'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('7'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('8'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('9'), KeyLength::SHORT),
         ],
         vec![
-            Key::from_values('Q', KeyLength::SHORT),
-            Key::from_values('W', KeyLength::SHORT),
-            Key::from_values('E', KeyLength::SHORT),
-            Key::from_values('R', KeyLength::SHORT),
-            Key::from_values('T', KeyLength::SHORT),
-            Key::from_values('Y', KeyLength::SHORT),
-            Key::from_values('U', KeyLength::SHORT),
-            Key::from_values('I', KeyLength::SHORT),
-            Key::from_values('O', KeyLength::SHORT),
-            Key::from_values('P', KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('Q'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('W'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('E'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('R'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('T'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('Y'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('U'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('I'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('O'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('P'), KeyLength::SHORT),
         ],
         vec![
-            Key::from_values('A', KeyLength::SHORT),
-            Key::from_values('S', KeyLength::SHORT),
-            Key::from_values('D', KeyLength::SHORT),
-            Key::from_values('F', KeyLength::SHORT),
-            Key::from_values('G', KeyLength::SHORT),
-            Key::from_values('H', KeyLength::SHORT),
-            Key::from_values('J', KeyLength::SHORT),
-            Key::from_values('K', KeyLength::SHORT),
-            Key::from_values('L', KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('A'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('S'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('D'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('F'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('G'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('H'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('J'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('K'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('L'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Enter, KeyLength::LONG),
         ],
         vec![
-            Key::from_values('Z', KeyLength::SHORT),
-            Key::from_values('X', KeyLength::SHORT),
-            Key::from_values('C', KeyLength::SHORT),
-            Key::from_values(' ', KeyLength::LONG),
-            Key::from_values('V', KeyLength::SHORT),
-            Key::from_values('B', KeyLength::SHORT),
-            Key::from_values('N', KeyLength::SHORT),
-            Key::from_values('M', KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('Z'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('X'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('C'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('_'), KeyLength::LONG),
+            Key::from_values(KeyCode::Char('V'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('B'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('N'), KeyLength::SHORT),
+            Key::from_values(KeyCode::Char('M'), KeyLength::SHORT),
         ],
     ]
+}
+
+#[allow(dead_code)]
+fn initialize_key_coord_map() -> HashMap<KeyCode, Coord> {
+    HashMap::from([
+        (KeyCode::Char('0'), (0, 0)),
+        (KeyCode::Char('1'), (0, 1)),
+        (KeyCode::Char('2'), (0, 2)),
+        (KeyCode::Char('3'), (0, 3)),
+        (KeyCode::Char('4'), (0, 4)),
+        (KeyCode::Char('5'), (0, 5)),
+        (KeyCode::Char('6'), (0, 6)),
+        (KeyCode::Char('7'), (0, 7)),
+        (KeyCode::Char('8'), (0, 8)),
+        (KeyCode::Char('9'), (0, 9)),
+        // Second row
+        (KeyCode::Char('Q'), (1, 0)),
+        (KeyCode::Char('W'), (1, 1)),
+        (KeyCode::Char('E'), (1, 2)),
+        (KeyCode::Char('R'), (1, 3)),
+        (KeyCode::Char('T'), (1, 4)),
+        (KeyCode::Char('Y'), (1, 5)),
+        (KeyCode::Char('U'), (1, 6)),
+        (KeyCode::Char('I'), (1, 7)),
+        (KeyCode::Char('O'), (1, 8)),
+        (KeyCode::Char('P'), (1, 9)),
+        // Third row
+        (KeyCode::Char('A'), (2, 0)),
+        (KeyCode::Char('S'), (2, 1)),
+        (KeyCode::Char('D'), (2, 2)),
+        (KeyCode::Char('F'), (2, 3)),
+        (KeyCode::Char('G'), (2, 4)),
+        (KeyCode::Char('H'), (2, 5)),
+        (KeyCode::Char('J'), (2, 6)),
+        (KeyCode::Char('K'), (2, 7)),
+        (KeyCode::Char('L'), (2, 8)),
+        (KeyCode::Enter, (2, 9)),
+        // Third row
+        (KeyCode::Char('Z'), (3, 0)),
+        (KeyCode::Char('X'), (3, 1)),
+        (KeyCode::Char('C'), (3, 2)),
+        (KeyCode::Char('V'), (3, 3)),
+        (KeyCode::Char('_'), (3, 4)),
+        (KeyCode::Char('Z'), (3, 0)),
+        (KeyCode::Char('Z'), (3, 0)),
+        (KeyCode::Char('Z'), (3, 0)),
+        (KeyCode::Char('Z'), (3, 0)),
+    ])
 }
 
 #[derive(Clone, Debug)]
@@ -147,7 +199,7 @@ fn center_rect(area: Rect, horizontal: Constraint, vertical: Constraint) -> Rect
     area
 }
 
-fn generate_keyboard_layout(
+fn generate_keyboard(
     area: Rect,
     horizontal: Constraint,
     vertical: Constraint,
@@ -177,7 +229,7 @@ fn generate_key_layout(key_layers: Rc<[Rect]>, keys: &Vec<Vec<Key>>) -> Vec<Rc<[
     for key_sub_vec in keys.into_iter() {
         let rat_vec: Vec<(u32, u32)> = key_sub_vec
             .into_iter()
-            .map(|element| match element.keylength {
+            .map(|element| match element.key_length {
                 KeyLength::SHORT => (1_u32, 10_u32),
                 KeyLength::MEDIUM => (2_u32, 10_u32),
                 KeyLength::LONG => (3_u32, 10_u32),
@@ -195,42 +247,48 @@ fn generate_key_layout(key_layers: Rc<[Rect]>, keys: &Vec<Vec<Key>>) -> Vec<Rc<[
     key_layout
 }
 
-#[allow(dead_code)]
-struct KeyboardLayout(Rect, Vec<Rc<[Rect]>>);
-
-#[allow(dead_code)]
-fn render_keyboard_layout(frame: &mut Frame, keys: &Vec<Vec<Key>>) -> KeyboardLayout {
+fn generate_keyboard_layout(frame: &mut Frame, keys: &Vec<Vec<Key>>) -> KeyboardLayout {
     let text_block = center_rect(
         frame.size(),
         Constraint::Percentage(70),
         Constraint::Length(4),
     );
 
-    let keyboard_layers = generate_keyboard_layout(
+    let keyboard_layers = generate_keyboard(
         frame.size(),
         Constraint::Percentage(75),
         Constraint::Length(frame.size().height / 3_u16),
         keys,
     );
 
-    frame.render_widget(Block::new().borders(Borders::all()), text_block);
+    KeyboardLayout(text_block, keyboard_layers)
+}
+
+struct KeyboardLayout(Rect, Vec<Rc<[Rect]>>);
+
+fn render_keyboard_layout(
+    frame: &mut Frame,
+    key_board_layout: &KeyboardLayout,
+    keys: &Vec<Vec<Key>>,
+) {
+    frame.render_widget(Block::new().borders(Borders::all()), key_board_layout.0);
 
     frame.render_widget(
         Paragraph::new(Text::from(String::from(
             "Welcome to the lyricist, ready to write your songs?",
         )))
         .centered(),
-        text_block,
+        key_board_layout.0,
     );
 
-    for key_layer in keyboard_layers.iter() {
+    for key_layer in key_board_layout.1.iter() {
         for key_rect in key_layer.iter() {
             frame.render_widget(Block::new().borders(Borders::all()), *key_rect);
         }
     }
 
     for (i, key_sub_vec) in keys.iter().enumerate() {
-        let key_sub_rect = match keyboard_layers.get(i) {
+        let key_sub_rect = match key_board_layout.1.get(i) {
             Some(r) => r,
             None => panic!(
                 "Failed to get the required rect, it should exist, please check what is wrong"
@@ -239,13 +297,11 @@ fn render_keyboard_layout(frame: &mut Frame, keys: &Vec<Vec<Key>>) -> KeyboardLa
 
         for (key_index, key) in key_sub_vec.iter().enumerate() {
             let key_rect = key_sub_rect.get(key_index).unwrap();
-            let key_char: String = String::from(key.keychar.to_string());
+            let key_char: String = String::from(key.key_code.to_string());
 
             frame.render_widget(Paragraph::new(key_char).centered(), *key_rect);
         }
     }
-
-    KeyboardLayout(text_block, keyboard_layers)
 }
 
 #[allow(dead_code)]
@@ -277,10 +333,15 @@ async fn main() -> Result<()> {
     let (sn, rc) = async_std::channel::unbounded::<keyboard_event::KeyPressEvent>();
 
     let _ = terminal.clear();
-    let keys = initialize_keys();
+
+    let keys = initialize_key_vec();
+    let key_map = initialize_key_coord_map();
+
+    let keyboard_layout: KeyboardLayout =
+        generate_keyboard_layout(&mut terminal.get_frame(), &keys);
 
     let _ = terminal.draw(|f| {
-        let _ = render_keyboard_layout(f, &keys);
+        let _ = render_keyboard_layout(f, &keyboard_layout, &keys);
     });
 
     loop {
@@ -290,10 +351,6 @@ async fn main() -> Result<()> {
             Ok(rec_eve) => state_struct.process_event(rec_eve),
             Err(e) => panic!("Failed to recieve the keyboard event, {}", e.to_string()),
         };
-
-        let _ = terminal.draw(|f| {
-            let _ = render_keyboard_layout(f, &keys);
-        });
 
         if quit {
             break;
