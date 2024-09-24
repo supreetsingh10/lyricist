01a15fd5a056b9d45f3c5d44d2ebbcc60d057466
Supreet <singh.supreet14@gmail.com>
2024-09-24 20:15:18 +0530

    Controls added and refactoring code

--- src/keyboard_event.rs
+++ src/keyboard_event.rs
@@ -1,17 +1,54 @@
 use core::panic;
+use crossterm::event::KeyCode;
 use std::time::Duration;
 
-use crossterm::event::{Event, EventStream, KeyEvent};
+use crossterm::event::{Event, EventStream, KeyEvent, KeyModifiers};
 use futures::{future::FutureExt, select, StreamExt};
 use futures_timer::Delay;
 
+#[allow(dead_code)]
 #[derive(PartialEq, Debug)]
-pub enum KeyPressEvent {
-    KeyPress(KeyEvent),
+pub enum Actions {
+    EXIT,
+    SEARCH,
+    TYPE,
+    PAUSE,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct KeyboardActions {
+    key_event: KeyEvent,
+    action: Actions,
+}
+
+impl KeyboardActions {
+    fn new(kkey_event: KeyEvent, k_action: Actions) -> Self {
+        KeyboardActions {
+            key_event: kkey_event,
+            action: k_action,
+        }
+    }
+
+    fn process_keyevent_for_actions(key_event: &KeyEvent) -> Self {
+        if key_event.eq(&KeyEvent::new(KeyCode::Char('s'), KeyModifiers::CONTROL)) {
+            return KeyboardActions::new(key_event.to_owned(), Actions::SEARCH);
+        } else if key_event.eq(&KeyEvent::new(KeyCode::Esc, KeyModifiers::NONE)) {
+            return KeyboardActions::new(key_event.to_owned(), Actions::EXIT);
+        } else if key_event.eq(&KeyEvent::new(KeyCode::Char('p'), KeyModifiers::CONTROL)) {
+            return KeyboardActions::new(key_event.to_owned(), Actions::PAUSE);
+        } else {
+            return KeyboardActions::new(key_event.to_owned(), Actions::TYPE);
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+pub enum KeyboardEvent {
+    KeyPress(KeyboardActions),
     NoPress,
 }
 
-pub async fn handle_keyboard_events(sn: async_std::channel::Sender<KeyPressEvent>) {
+pub async fn handle_keyboard_events(sn: async_std::channel::Sender<KeyboardEvent>) {
     let mut event_tapper = EventStream::new();
 
     loop {
@@ -20,19 +57,19 @@ pub async fn handle_keyboard_events(sn: async_std::channel::Sender<KeyPressEvent
 
         select! {
             _ = delay => {
-                let _ = sn.send(KeyPressEvent::NoPress);
+                let _ = sn.send(KeyboardEvent::NoPress);
             },
             maybe_event = event => {
                 match maybe_event {
                     Some(Ok(event)) => {
                         let _ = match event {
                             Event::Key(k) => {
-                                if let Err(e) = sn.send(KeyPressEvent::KeyPress(k)).await {
+                                if let Err(e) = sn.send(KeyboardEvent::KeyPress(KeyboardActions::process_keyevent_for_actions(&k))).await {
                                     panic!("Failed to send {}", e.to_string());
                                 }
                             }
                             _ => {
-                                if let Err(e) = sn.send(KeyPressEvent::NoPress).await {
+                                if let Err(e) = sn.send(KeyboardEvent::NoPress).await {
                                     panic!("Failed to send {}", e.to_string());
                                 }
                             },
